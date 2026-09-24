8b76fbda31e8dd5bfd93f9935ebb5265f9b475ff
digitaltrails <michael@actrix.gen.nz>
2026-09-24 14:31:39 +1200

    Created some dbus examples

---
+++ dbus-frontend/example-clients/dbus-examples.bash
@@ -0,0 +1,5 @@
+busctl --user call com.ddcutil.DdcutilService /com/ddcutil/DdcutilObject com.ddcutil.DdcutilInterface SetVcp isyqu 1 "" 0x10  90  0
+busctl --user call com.ddcutil.DdcutilService /com/ddcutil/DdcutilObject com.ddcutil.DdcutilInterface SetVcpWithContext isyqsu 1 "" 0x10  60 "my_app" 0
+busctl --user call com.ddcutil.DdcutilService /com/ddcutil/DdcutilObject com.ddcutil.DdcutilInterface GetVcp isyu 1 "" 0x10 0
+busctl --user call com.ddcutil.DdcutilService /com/ddcutil/DdcutilObject com.ddcutil.DdcutilInterface GetMultipleVcp isayu 1 "" 2 0x10 0x12 0
+busctl --user call com.ddcutil.DdcutilService /com/ddcutil/DdcutilObject com.ddcutil.DdcutilInterface GetVcpMetadata isyu 1 "" 0x10 0
